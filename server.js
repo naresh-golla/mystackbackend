cb04313a4fb460a8e5fe50bbf42e4d5d073a0044
varun2116 <varun2116@gmail.com>
2019-08-20 10:37:01 +0000

    Update User Info

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var UserInfo = require('./models/userInfo');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
 
 app.get('/getUser/:username', (req, res) => {
     UserInfo.findOne({ username: req.params.username }, (err, info) => {
@@ -60,6 +66,6 @@ mongoose.connect(dbUrl.devUrl, (err) => {
     console.error("mongoDb Connection", err);
 });
 
-var server = http.listen(3000, () => {
+var server = http.listen(8000, () => {
     console.log('server is listening to the port ', server.address().port);
 });
